ce1c7023c8a859fcdabf775c180d60f362ddc017
sahithi <sdudduk1@in.ibm.com>
2018-12-27 19:11:20 +0530

    Add

--- src/test/java/com/stackroute/DateofaweekTest.java
+++ src/test/java/com/stackroute/DateofaweekTest.java
@@ -1,13 +1,10 @@
-package com.stackroute;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-public class DateofaweekTest {
+package com.stackroute;import org.junit.Test;
+import static org.junit.Assert.*;public class DateofaweekTest {
     Dateofaweek d = new Dateofaweek();    @Test
     public void caltest() {
-        assertEquals("First day of week : Monday 24/12/2018 Last day of week : 30/12/2018", d.calender("dd/MM/YYYY"));
+        assertEquals("First day of week : Monday 24/12/2018 Last day of week : Sunday 30/12/2018", d.calender("dd/MM/YYYY"));
     }    @Test
     public void caltest1() {
-        assertNotEquals("Last day of week : Monday 24/12/2015 Last day of week : 30/12/2015",d.calender("dd/mm/yyyy"));
+        assertNotEquals("Last day of week : Monday 24/12/2015 Last day of week : Sunday 30/12/2015",d.calender("dd/mm/yyyy"));
     }
 }
