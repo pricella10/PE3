47bb0a3d2c67199de8286b6fe7d13bd1138e05e9
pricella10 <chintusonu55@gmail.com>
2018-12-27 15:41:25 +0530

    commited 2

--- src/test/java/com/stackroute/MatrixTest.java
+++ src/test/java/com/stackroute/MatrixTest.java
@@ -4,23 +4,33 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MatrixTest {
+
     Matrix mat = new Matrix();
 
     @Test
     public void matrixSum()
     {
-        int[][] result = mat.add(new int[][]{{1, 2, 3}, {4, 5, 6}}, new int[][]{{9, 8, 7}, {6, 5, 4}});
+        int[][] result = mat.sum(new int[][]{{1, 2, 3}, {4, 5, 6}}, new int[][]{{9, 8, 7}, {6, 5, 4}});
         assertArrayEquals(new int[][]{{10,10},{10,10},{10,10}},result);
     }
 
-
-
+    @Test
+    public void matrixSum1()
+    {
+        int[][] result = mat.sum(new int[][]{{1, 2, 3}, {4, 5, 6},{7,8,9}}, new int[][]{{9, 8, 7}, {6, 5, 4},{1,2,3}});
+        assertArrayEquals(new int[][]{{24,24,24},{72,69,66},{120,114,108}},result);
+    }
+    @Test
+    public void matrixSum2()
+    {
+        int[][] result = mat.sum(new int[][]{{1,2},{3,4}}, new int[][]{{4,3},{2,1}});
+        assertArrayEquals(new int[][]{{5,5},{5,5}},result);
+    }
     @Test
     public void matrixSumFail()
     {
-        int[][] result = mat.add(new int[][]{{1, 2, 3}, {4, 5, 6}}, new int[][]{{9, 8, 7}, {6, 5, 4}});
-        assertArrayEquals(new int[][]{{10,10},{9,10},{10,10}},result);
+        int[][] result = mat.sum(new int[][]{{1, 2, 3}, {4, 5, 6}}, new int[][]{{9, 8, 7}, {6, 5, 4}});
+        assertArrayEquals(new int[][]{{10,10},{10,10}},result);
     }
-
 
 }
--- src/test/java/com/stackroute/VowelsTest.java
+++ src/test/java/com/stackroute/VowelsTest.java
@@ -1,17 +1,30 @@
 package com.stackroute;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
-public class VowelsTest {
-    Vowels vowel = new Vowels();
+public class VowelTest {
+
+    Vowel vowel = new Vowel();
+
 
     @Test
-    public void removeVowels()
+    public void remove()
     {
-        String[] result= vowel.vowelRemover(new String[]{"India,United States, Germany,Egypt,czechoslovakia"});
-        assertArrayEquals(new String[]{"Ind,Untd Stts,Grmny,Egypt,czchslvk"},result);
+        String[] result = vowel.removeVowel(new String[]{"India","United States","Germany","Egypt","czechoslovakia"});
+        assertArrayEquals(new String[]{"Ind","Untd Stts","Grmny","Egypt","czchslvk"},result);
     }
 
+    @Test
+    public void remove1()
+    {
+        String[] result = vowel.removeVowel(new String[]{"Egypt,czechoslovakia,India,United States,Germany"});
+        assertArrayEquals(new String[]{"Egypt,czchslvk,Ind,Untd Stts,Grmny"},result);
+    }
+    @Test
+    public void removeFail()
+    {
+        String[] result = vowel.removeVowel(new String[]{"India,czechoslovakia"});
+        assertArrayEquals(new String[]{"Ind,czchslvk"},result);
+    }
 }
