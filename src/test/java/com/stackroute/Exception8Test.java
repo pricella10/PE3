618ebfeb8b5e3be93a8bdf931ec9b3495322f7e7
Rachan <45649992+RachanSaini@users.noreply.github.com>
2018-12-28 09:37:00 +0530

    Exception8Test file

---
+++ src/test/java/com/stackroute/Exception8Test.java
@@ -0,0 +1,47 @@
+package com.stackroute;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Exception8Test {
+
+    private Exception8 exceptionObj = new Exception8();
+
+    //NegativeArraySizeException
+    @Test
+    public void negativeArrayTest(){
+        String expectedValue = "class java.lang.NegativeArraySizeException";
+        String actualValue = exceptionObj.negativeArray();
+        assertEquals(expectedValue,actualValue);
+
+
+    }
+
+    //IndexOutOfBoundsException
+
+
+    @Test
+    public void indexOutOfBoundArray(){
+        String expectedValue = "class java.lang.ArrayIndexOutOfBound";
+        String actualValue = exceptionObj.indexOutOfBoundArray();
+        assertEquals(expectedValue,actualValue);
+
+
+    }
+
+//NullPointerException
+    @Test
+    public void indexOutOfBoundArray(){
+        String expectedValue = "class java.lang.NullPointerException";
+        String actualValue = exceptionObj.NullPointerException();
+        assertEquals(expectedValue,actualValue);
+
+
+    }
+
+
+
+
+}
+
